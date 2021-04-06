cac10066c0f7afaf71e7d2e726203cb5c6c9af6a
saidMounaim <saidmounaim00@gmail.com>
2021-04-06 13:06:34 +0100

    Updates

---
+++ precache-manifest.71bc726f1b62de12a21b1608b0e6153f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06668cf0ea9bba95e17c6a6f7ddb30d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "58f60e36c9453196893d",
+    "url": "./static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "087e0b65b57be9a518a3",
+    "url": "./static/js/2.b7464675.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b7464675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f60e36c9453196893d",
+    "url": "./static/js/main.5496ee9a.chunk.js"
+  },
+  {
+    "revision": "82839a07d846b7f6760e",
+    "url": "./static/js/runtime-main.7d8e4955.js"
+  }
+]);
